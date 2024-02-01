8b3c889d7220a127aa9a67b4305eabebfece93bb
Rohithv17 <rohithv1396@gmail.com>
2024-02-01 11:15:31 +0530

    Added functionality to generate Random number to calculate player moves

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-
+import com.bridgelabz.SnakeAndLadder.PlayerMoves;
 
 class main {
 
     public static void main(String[] args) {
+        PlayerMoves p= new PlayerMoves();
+        p.compute();
 
     }
 
--- src/com/bridgelabz/SnakeAndLadder/PlayerMoves.java
+++ src/com/bridgelabz/SnakeAndLadder/PlayerMoves.java
@@ -1,6 +1,14 @@
 package com.bridgelabz.SnakeAndLadder;
-
+import java.util.Random;
 public class PlayerMoves {
 
-    int pos=0;  //refers to  current position of player
+    public int pos=0;  //refers to  current position of player
+
+   public void compute(){
+        Random rn= new Random();
+        int rolldie= rn.nextInt(6);   //Generates a random number between 1-6 which can used to calculate player moves
+        System.out.println(rolldie);
+
+
+    }
 }
